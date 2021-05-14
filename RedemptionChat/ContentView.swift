ff31daa5560378d07f0f894afa17ee7811684556
Max <maxkrefting>
2021-05-14 11:08:46 +0100

    Initial Commit

---
+++ RedemptionChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RedemptionChat
+//
+//  Created by Krefting, Max (PGW) on 14/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RedemptionChat/RedemptionChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedemptionChatApp.swift
+//  RedemptionChat
+//
+//  Created by Krefting, Max (PGW) on 14/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct RedemptionChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
